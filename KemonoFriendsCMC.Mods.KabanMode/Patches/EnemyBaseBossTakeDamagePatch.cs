a24f6929e561ecc60a84709546110882ca8c1298
pri-san <88888437+pri-san@users.noreply.github.com>
2021-08-14 17:23:38 +0900

    かばんちゃん操作でもボス攻略できるよう修正

---
+++ KemonoFriendsCMC.Mods.KabanMode/Patches/EnemyBaseBossTakeDamagePatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace KemonoFriendsCMC.Mods.KabanMode.Patches
+{
+    [HarmonyPatch(typeof(EnemyBaseBoss), "TakeDamage")]
+    public static class EnemyBaseBossTakeDamagePatch
+    {
+        public static void Prefix(
+            EnemyBaseBoss __instance,
+            ref CharacterBase attacker
+            )
+        {
+            if (!ConfigValues.Enabled.Value)
+            {
+                return;
+            }
+            // 他のフレンズの攻撃でもトドメをさせるよう修正
+            if (__instance.GetNowHP() == 1)
+            {
+                attacker = null;
+            }
+        }
+    }
+}
